db77a0e940f9dff9fbd6443d67b10976fd9d5446
vanoos-git <vanobond2@gmail.com>
2016-11-18 00:26:00 +0300

    asd

--- js/code2.js
+++ js/code2.js
@@ -1,8 +1,8 @@
 var arrange = function(data) {
-var template = Handlebars.compile($('#template').html());
-$('.services').append(template(data));
-});
-}
+    var template = Handlebars.compile($('#template').html());
+    $('.services').append(template(data));
+};
+
 
 var request = new XMLHttpRequest();
 request.open('GET', 'base2.json', true);
