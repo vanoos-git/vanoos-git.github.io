5d72f28f9fe4dc4ce94228aa8e39aa44e1453b1e
vanoos-git <vanobond2@gmail.com>
2016-11-18 00:16:52 +0300

    handlebars-v4.0.5.js

---
+++ js/code2.js
@@ -0,0 +1,22 @@
+var arrange = function(data) {
+var template = Handlebars.compile($('#template').html());
+$('.services').append(template(data));
+});
+}
+
+var request = new XMLHttpRequest();
+request.open('GET', 'base2.json', true);
+
+request.onload = function() {
+    if (request.status == 200) {
+        var data = JSON.parse(request.responseText);
+        arrange(data);
+    } else {
+        console.log("error")
+    }
+};
+request.onerror = function() {
+    // There was a connection error of some sort
+};
+
+request.send();
