cabf6e2d406d95db2370664784f765e57db7d087
vanoos-git <vanobond2@gmail.com>
2016-11-21 02:34:13 +0300

    asd

--- js/code2.js
+++ js/code2.js
@@ -30,11 +30,11 @@ $(document).ready(function() {
     });
     var Second = function() {
         $('button.fbutton').prop("disabled", false).click(function() {
+            $('.b-popup-content:').remove();
             getjson("JSON/" + $(this).data("num") + ".json", HBcompile, "#ShowJson", ".b-popup").done(function() {
-                if (ERR === 0) {
-                    $('.b-popup').fadeIn(300);
+                
                 }
-              $('.b-popup-content:first').remove();  
+
             });
         });
     }
